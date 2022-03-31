ebd363b0bb6a95d0b58fa78b855af15c3d61bc06
Mike Cunningham <mike@expertcoders.net>
2022-03-31 14:42:01 -0500

    Added some comments.

--- alarm.py
+++ alarm.py
@@ -4,16 +4,17 @@ from time import sleep
 
 while 1:
     try:
-        response = urllib.request.urlopen('https://expertcoders.net')
-        break        
+        response = urllib.request.urlopen('https://expertcoders.net') #Try to connect to a website
+        break #If it does not throw an error break out of loop
     except:
+        #Couldn't connect. Error thrown
         print("Internet Not Connected")
         sleep(5)
         subprocess.call("clear")
         print("Checking Internet")
     sleep(10)    
-while 1:
-    subprocess.call("bash noise.sh", shell=True)
+while 1: #Internet must be on because we are here
+    subprocess.call("bash noise.sh", shell=True) #Make noise
     print("Internet is now working")
     sleep(1)
     
