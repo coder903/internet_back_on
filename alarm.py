dc03dfa6498747df5082be4213a7d2361c949a5d
Shannon Cody Russell <48037883+uncoded0123@users.noreply.github.com>
2023-01-04 22:16:52 -0600

    fixed, changed line 16

--- alarm.py
+++ alarm.py
@@ -4,16 +4,17 @@ from time import sleep
 
 while 1:
     try:
-        response = urllib.request.urlopen('https://expertcoders.net') # try to connect to a website
+        response = urllib.request.urlopen('https://expertcoders.net') #Try to connect to a website
         if platform.system() == 'Windows':
-            Dispatch("SAPI.Spvoice").Speak("Internet is on!")
+            Dispatch("SAPI.Spvoice").Speak("The internet is on!")
         else:
             subprocess.call("bash noise.sh", shell=True)
 
     except:
         # not connected, so we're here
         if platform.system() == 'Windows':
-            Dispatch("SAPI.Spvoice").Speak("Internet is off")
+            print("Internet is off")
         else:
             subprocess.call("clear")
+
     sleep(10)
